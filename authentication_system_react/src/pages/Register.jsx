178ddb8043d3d05fd58bf30c7db32a82d8f890fb
madhuraj <madhurajmadu693@gmail.com>
2025-04-22 13:01:58 +0530

    registering user and login functionality is improved

--- authentication_system_react/src/pages/Register.jsx
+++ authentication_system_react/src/pages/Register.jsx
@@ -4,49 +4,197 @@ import { register } from "../services/api";
 import { toast } from "react-toastify";
 
 export default function Register() {
-  const [form, setForm] = useState({ fullname: "", email: "", password: "" });
+  const [form, setForm] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    employeeId: "",
+    jobTitle: "",
+    department: "",
+    phoneNumber: "",
+  });
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   //validation form
+  // const validateForm = () => {
+  //   if (!form.fullname) {
+  //     toast.error("Full name is required");
+  //     setForm((prev) => ({ ...prev, fullname: "" }));
+
+  //     return false;
+  //   }
+
+  //   if (!form.email) {
+  //     toast.error("Email is required");
+  //     setForm((prev) => ({ ...prev, email: "" }));
+
+  //     return false;
+  //   } else if (!/\S+@\S+\.\S+/.test(form.email)) {
+  //     toast.error("Enter a valid email");
+  //     setForm((prev) => ({ ...prev, email: "" }));
+
+  //     return false;
+  //   }
+
+  //   if (!form.password) {
+  //     toast.error("Password is required");
+  //     setForm((prev) => ({ ...prev, password: "" }));
+  //     return false;
+  //   } else if (form.password.length < 6) {
+  //     toast.error("Password must be at least 6 characters");
+  //     setForm((prev) => ({ ...prev, password: "" }));
+  //     return false;
+  //   } else if (!/[A-Z]/.test(form.password)) {
+  //     toast.error("Password must contain at least one uppercase letter");
+  //     setForm((prev)=>({...prev, password:""}))
+  //     return false;
+  //   } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(form.password)) {
+  //     toast.error(
+  //       "Password must contain at least one special character (e.g., !@#$%^&*(),.?)"
+  //     );
+  //     setForm((prev) => ({ ...prev, password: "" }));
+  //     return false;
+  //   }
+
+  //   if (!form.confirmPassword) {
+  //     toast.error("Confirm Password is required");
+  //     setForm((prev) => ({ ...prev, confirmPassword: "" }));
+  //     return false;
+  //   } else if (form.password !== form.confirmPassword) {
+  //     toast.error("Passwords do not match");
+  //     setForm((prev) => ({ ...prev, confirmPassword: "" }));
+  //     return false;
+  //   }
+
+  //   if (!form.employeeId.trim()) {
+  //     toast.error("Employee ID is required");
+  //     setForm((prev) => ({ ...prev, employeeId: "" }));
+  //     return false;
+  //   }
+
+  //   if (!form.jobTitle.trim()) {
+  //     toast.error("Job Title is required");
+  //     setForm((prev) => ({ ...prev, jobTitle: "" }));
+  //     return false;
+  //   }
+
+  //   if (!form.department.trim()) {
+  //     toast.error("Department is required");
+  //     setForm((prev) => ({ ...prev, department: "" }));
+  //     return false;
+  //   }
+
+  //   if (!form.phoneNumber.trim()) {
+  //     toast.error("Mobile number is required");
+  //     setForm((prev) => ({ ...prev, phoneNumber: "" }));
+  //     return false;
+  //   } else if (!/^\d{10}$/.test(form.phoneNumber)) {
+  //     toast.error("Mobile number must be 10 digits");
+  //     setForm((prev) => ({ ...prev, phoneNumber: "" }));
+  //     return false;
+  //   }
+
+  //   return true;
+  // };
+
   const validateForm = () => {
-    if (!form.fullname) {
+    const {
+      fullname,
+      email,
+      password,
+      confirmPassword,
+      employeeId,
+      jobTitle,
+      department,
+      phoneNumber,
+    } = form;
+
+    if (!fullname.trim()) {
       toast.error("Full name is required");
       return false;
     }
 
-    if (!form.email) {
+    if (!email.trim()) {
       toast.error("Email is required");
       return false;
-    } else if (!/\S+@\S+\.\S+/.test(form.email)) {
+    }
+
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
       toast.error("Enter a valid email");
       return false;
     }
 
-    if (!form.password) {
+    if (!password) {
       toast.error("Password is required");
       return false;
-    } else if (form.password.length < 6) {
+    }
+
+    if (password.length < 6) {
       toast.error("Password must be at least 6 characters");
       return false;
-    } else if (!/[A-Z]/.test(form.password)) {
+    }
+
+    if (!/[A-Z]/.test(password)) {
       toast.error("Password must contain at least one uppercase letter");
       return false;
-    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(form.password)) {
-      toast.error(
-        "Password must contain at least one special character (e.g., !@#$%^&*(),.?)"
-      );
+    }
+
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      toast.error("Password must contain at least one special character");
+      return false;
+    }
+
+    if (!confirmPassword) {
+      toast.error("Confirm Password is required");
+      return false;
+    }
+    console.log(password+"password");
+    console.log(confirmPassword+"confirm password");
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return false;
+    }
+
+    if (!employeeId.trim()) {
+      toast.error("Employee ID is required");
+      return false;
+    }
+
+    if (!jobTitle.trim()) {
+      toast.error("Job Title is required");
+      return false;
+    }
+
+    if (!department.trim()) {
+      toast.error("Department is required");
+      return false;
+    }
+
+    if (!phoneNumber.trim()) {
+      toast.error("Mobile number is required");
+      return false;
+    }
+
+    const phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phoneNumber)) {
+      toast.error("Mobile number must be 10 digits");
       return false;
     }
 
     return true;
   };
 
+  console.log(form);
   const handleSubmit = async (e) => {
     e.preventDefault();
     // setMessage("");
@@ -60,12 +208,29 @@ export default function Register() {
       setTimeout(() => navigate("/login"), 1500);
     } catch (err) {
       const errorMsg = err.response?.data?.message;
-      toast.error(
-        "Invalid input. Please check your email or password." + errorMsg
-      );
+      if (errorMsg?.includes("email")) {
+        toast.error("Email already exists. Please use a different one.");
+      } else if (errorMsg?.includes("employeeId")) {
+        toast.error("Employee ID already exists. Please use a different one.");
+      } else {
+        toast.error(
+          "Invalid input. Please check your email or password." + errorMsg
+        );
+      }
+
+      setForm({
+        fullname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        employeeId: "",
+        jobTitle: "",
+        department: "",
+        phoneNumber: "",
+      });
     }
   };
-// work with registwer user 
+  // work with registwer user
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-100 to-purple-200">
       <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-lg">
@@ -111,7 +276,7 @@ export default function Register() {
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
             required
           />
-          <input
+          {/* <input
             type="password"
             name="password"
             placeholder="Password"
@@ -119,7 +284,80 @@ export default function Register() {
             onChange={handleChange}
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
             required
+          /> */}
+          <div className="relative w-full">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+              value={form.password}
+              onChange={handleChange}
+              className="w-full px-4 py-2 pr-10 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+              required
+            />
+            {/* <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 focus:outline-none"
+            >
+              {showPassword ? "Disable" : "Show Password"}
+            </button> */}
+          </div>
+          <label className="flex items-center mt-2 space-x-2 text-sm text-gray-600">
+            <input
+              type="checkbox"
+              checked={showPassword}
+              onChange={() => setShowPassword(!showPassword)}
+              className="form-checkbox"
+            />
+            <span>Show Password</span>
+          </label>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            value={form.confirmPassword}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+            required
           />
+          <input
+            type="text"
+            name="employeeId"
+            placeholder="Employee ID"
+            value={form.employeeId}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+            required
+          />
+          <input
+            type="text"
+            name="jobTitle"
+            placeholder="Job Title"
+            value={form.jobTitle}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+            required
+          />
+          <input
+            type="text"
+            name="department"
+            placeholder="Department"
+            value={form.department}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+            required
+          />
+          <input
+            type="number"
+            name="phoneNumber"
+            placeholder="Mobile Number"
+            value={form.phoneNumber}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+            required
+          />
+
           <button
             type="submit"
             className="w-full bg-gradient-to-r from-purple-600 to-blue-600 text-white py-3 px-4 rounded-lg font-medium transition-all duration-200 ease-in-out hover:opacity-90 focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
